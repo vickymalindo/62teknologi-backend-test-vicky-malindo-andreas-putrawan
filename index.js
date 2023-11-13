b8de1b2c905f4540bb8602e6b2fb88e5b9beb1f2
vickymalindo <vicky.malindo@gmail.com>
2023-11-13 14:04:19 +0700

    feat: implement route

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import db from './config/database.js';
-import Business from './models/business.js';
+import router from './routes/route.js';
 
 const app = express();
 
 try {
   await db.authenticate();
   console.log('Database connected');
-  await Business.sync({ force: true });
 } catch (error) {
   console.log(error);
 }
 
 app.use(express.json());
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Example app listening on 3000');
